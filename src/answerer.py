fe6e5ec39c727cd7ae5bf6f9d7ae14eb57f77ff5
George Ke <george-ke@hotmail.com>
2016-01-12 23:13:34 -0500

    clean up code and code structure + store lyric_dict

--- src/answerer.py
+++ src/answerer.py
@@ -1,78 +1,20 @@
 import json
 import numpy as np
 import nltk
+import data.lyric_formatter as lyric_formatter
 from nltk.corpus import wordnet as wn
 
-with open("./data/lyrics_subset.json", "r") as file:
-    lyrics = json.load(file)["lyrics"]
+# hardcode for now.
+INPUT = "What should I add to my curry"
 
-distance_matrix = np.zeros((len(lyrics), len(lyrics)))
-
-def get_word_map(l):
-    tokens = nltk.word_tokenize(l)
-    tagged = nltk.pos_tag(tokens)
-
-    # TODO() assign weights to each of these?
-    # JJ: adjective or numeral, ordinalResource 'tokenizers/punkt/english.pickle' not found.
-    # JJR: adjective, comparative
-    # JJS: adjective, superlative
-    # NN: noun
-    # NNS: noun, plural
-    # RB: adverb
-    # RBR: adverb, comparative
-    # RBS: adverb, superlative
-    # VB: verb, base
-    # VBD: verb, past tense
-    # VBG: verb, present participle
-    # VBN: verb, past participle
-    # VBP: verb, present tense, not 3rd person singular
-    # VBZ: ^                        ^
-    # other potential tags:
-    #   CD: numeral, cardinal
-    #   NNP: proper nouns
-    #   MD: modal auxiliary (can, couldn't, will, would)
-    #   PRP: pronoun, personal
-    #   PRP$: pronoun, possessive
-    #   UH: interjection
-    tags = ["JJ", "JJR", "JJS", "NN", "NNS", "RB", "RBR",
-            "RBS", "VB", "VBD", "VBG", "VBN", "VBP", "VBZ"]
-    word_dict = {
-        "N": [],
-        "R": [],
-        "V": [],
-        "J": []
-    }
-    count = 0
-    for tup in tagged:
-        for k in word_dict.keys():
-            if tup[1][0] == k:
-                count += 1
-                word_dict[k].append(tup[0])
-                break
-    word_dict["count"] = count
-    return word_dict
-
-# TODO: store in db
-# add keywords to each lyric
-lydict = {}
-max_line_count = float("-inf")
-for l in lyrics:
-    print(l)
-    lydict[l] = get_word_map(l)
-    if lydict[l]["count"] > max_line_count:
-        max_line_count = lydict[l]["count"]
-
-# TODO: compute pairwise distances
-# for l1 in lyrics:
-#     for l2 in lyrics:
-#         if l1 != l2:
-#             pass
+with open("./data/json/lyric_dict.json", "r") as infile:
+    lydict = json.load(infile)
 
 dists = []
 
-input_map = get_word_map("What should I add to my curry")
+input_map = lyric_formatter.get_word_map(INPUT)
 
-stupid_array = {
+char_to_wordnet = {
     "N": wn.NOUN,
     "R": wn.ADV,
     "V": wn.VERB,
@@ -83,14 +25,13 @@ max_count = float("-inf")
 for lyric in lydict:
     dist = 0
     count = 0
-    #print(lyric)
-    for key in stupid_array:
+    for key in char_to_wordnet:
         for word1 in input_map[key]:
             best_dist = float("-inf")
             for word2 in lydict[lyric][key]:
                 try:
-                    a = wn.synsets(word1, pos=stupid_array[key])[0]
-                    b = wn.synsets(word2, pos=stupid_array[key])[0]
+                    a = wn.synsets(word1, pos=char_to_wordnet[key])[0]
+                    b = wn.synsets(word2, pos=char_to_wordnet[key])[0]
                     #print(a, b)
                     #print(a.path_similarity(b))
                     value = a.path_similarity(b)
