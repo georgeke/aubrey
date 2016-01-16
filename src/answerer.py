9353b862815b5e963c8da20a321d7f22240981d0
George Ke <george-ke@hotmail.com>
2016-01-16 16:48:56 -0500

    remove distance_calculator, dont need a sim. matrix anymore

--- src/answerer.py
+++ src/answerer.py
@@ -4,7 +4,51 @@ import time
 from data.clusterer import Clusterer
 from data.lyric_formatter import get_word_map
 from data.feature_matrix_calculator import convert_lyric_to_feature_set
-from data.distance_calculator import distance
+
+def distance(lyric_dict1, lyric_dict2):
+    """
+    Returns the distance between two lyrics, the greater the magnitude, the 'closer' the lyrics are
+    1. Calculate the path_similarity value for each pair of tokens (that have the same part of speech)
+       between lyric1 and lyric2
+    2. Sum them up and divide the number of pairings that added to that sum to normalize for lengths of lyrics
+       (i.e. a long lyric with a large sum should have a similar distance to a shorter lyric with a smaller sum.
+        This is to balance quality of matches with quantity of matches)
+    """
+    CHAR_TO_WORDNET = {
+        "N": wn.NOUN,
+        "R": wn.ADV,
+        "V": wn.VERB,
+        "J": wn.ADJ
+    }
+
+    dist = 0
+    count = 0
+    for key in CHAR_TO_WORDNET:
+        for word1 in lyric_dict1[key]:
+            best_dist = float("-inf")
+            for word2 in lyric_dict2[key]:
+                try:
+                    a = wn.synsets(word1, pos=CHAR_TO_WORDNET[key])[0]
+                    b = wn.synsets(word2, pos=CHAR_TO_WORDNET[key])[0]
+                    # Maximum returned by path_similarity is 1 (same words)
+                    value = a.path_similarity(b)
+                    if value is None:
+                        value = 0
+
+                    if value > best_dist:
+                        best_dist = value
+                except IndexError:
+                    continue
+            if np.isfinite(best_dist):
+                count += 1
+                dist += best_dist
+
+    if count == 0:
+        dist = float("-inf")
+
+    if np.isfinite(dist) and count > 0:
+        return dist / count
+    return None
 
 def answer(question):
     with open("data/json/lyric_dict.json", "r") as infile:
