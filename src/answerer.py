2dad894f18dd151c801260a111fc6538ada4dae1
George Ke <george-ke@hotmail.com>
2016-01-16 16:48:09 -0500

    complete pipeline starting from hardcoded question

--- src/answerer.py
+++ src/answerer.py
@@ -1,31 +1,39 @@
 import json
 import numpy as np
 import time
-import data.lyric_formatter as lyric_formatter
-import data.distance_calculator as distance_calculator
+from data.clusterer import Clusterer
+from data.lyric_formatter import get_word_map
+from data.feature_matrix_calculator import convert_lyric_to_feature_set
+from data.distance_calculator import distance
 
 def answer(question):
     with open("data/json/lyric_dict.json", "r") as infile:
-        lydict = json.load(infile)
+        lyric_dict = json.load(infile)
+
+    clusterer = Clusterer()
+    clusterer.generate_clusters()
+
+    input_map = get_word_map(question)
+    label = clusterer.predict([convert_lyric_to_feature_set(question, input_map)])[0]
+    relevant_lyrics = clusterer.cluster_map[label]
+    relevant_lyric_dict = {k:v for k, v in zip(relevant_lyrics, [lyric_dict[l] for l in relevant_lyrics])}
 
     dists = []
 
-    input_map = lyric_formatter.get_word_map(question)
-
     now = time.time()
 
-    for lyric in lydict:
+    for lyric in relevant_lyric_dict:
         dists.append({
             "lyric": lyric,
-            "dist": distance_calculator.distance(input_map, lydict[lyric])
+            "dist": distance(input_map, relevant_lyric_dict[lyric])
         })
 
-    min_dist = float("-inf")
+    max_dist = float("-inf")
     the_lyric = None
     for dist_obj in dists:
         a_dist = dist_obj["dist"]
-        if a_dist is not None and a_dist > min_dist:
-            min_dist = a_dist
+        if a_dist is not None and a_dist > max_dist:
+            max_dist = a_dist
             the_lyric = dist_obj["lyric"]
 
     then = time.time()
@@ -34,6 +42,6 @@ def answer(question):
 
 # For testing.
 if __name__ == "__main__":
-    the_lyric = answer("What should I eat for dinner?")
+    the_lyric = answer("What is my best friend's name")
     print("")
     print(the_lyric)
