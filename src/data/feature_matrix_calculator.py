c72a071147a8b480179eda8a43c7afb6a061a215
George Ke <george-ke@hotmail.com>
2016-01-16 17:03:35 -0500

    function to convert a lyric to a feature array

--- src/data/feature_matrix_calculator.py
+++ src/data/feature_matrix_calculator.py
@@ -10,6 +10,16 @@ Each row is a lyric, in the order they are stored in JSON, and each column is a 
 bag of words, ordered as they are in the JSON file.
 """
 
+def convert_lyric_to_feature_set(lyric, its_dict, bag_of_words):
+    arr = [0] * len(bag_of_words)
+    word_counts = defaultdict(int)
+    for word in its_dict["words"]:
+        word_counts[word] += 1
+    for col, word_from_bag in enumerate(bag_of_words):
+        if word_counts[word_from_bag] > 0:
+            arr[col] = word_counts[word_from_bag]
+    return arr
+
 if __name__ == "__main__":
     with open("json/bag_of_words.json", "r") as infile:
         bag_of_words = json.load(infile)
@@ -17,15 +27,10 @@ if __name__ == "__main__":
     with open("json/lyric_dict.json", "r") as infile:
         lyric_dict = json.load(infile)
 
-    feature_matrix = np.zeros((len(lyric_dict), len(bag_of_words)))
+    feature_matrix = [[]] * len(lyric_dict)
 
     for (row, (lyric, its_dict)) in enumerate(lyric_dict.items()):
-        word_counts = defaultdict(int)
-        for word in its_dict["words"]:
-            word_counts[word] += 1
-        for col, word_from_bag in enumerate(bag_of_words):
-            if word_counts[word_from_bag] > 0:
-                feature_matrix[row][col] = word_counts[word_from_bag]
+        feature_matrix[row] = convert_lyric_to_feature_set(lyric, its_dict, bag_of_words)
 
     with open("json/feature_matrix.json", "w") as outfile:
         json.dump(feature_matrix.tolist(), outfile)
