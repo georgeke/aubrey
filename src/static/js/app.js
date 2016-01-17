cbc0affff9ad0a1e89b57fdbfd22cdbce7b0ff82
George Ke <george-ke@hotmail.com>
2016-01-16 22:30:32 -0500

    auto resizing text + show answer properly

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -5,17 +5,38 @@ var app = (function ($) {
     function callTheHotline() {
         event.preventDefault();
         var question = $("#question").val();
+        var description = $("#description");
 
         $.get("/hotline?question=" + question, function(data, status, response) {
             if (response.readyState == 4 && response.status == 200) {
-            $("#bling").text(response.responseText);
+                $("#bling").text(response.responseText).show();
+                description.text(DEFAULT_DESC);
+                description.removeClass("flashing");
+                description.hide();
+                $("body div img").hide();
+                $("#question").prop('disabled', true);
             }
         });
 
-        var description = $("#description")
-        description.text(LOADING_DESC);
-        description.addClass("flashing");
+        $("button").prop('disabled', true);
+        description.text(LOADING_DESC).addClass("flashing");
     }
+
+    $( document ).ready(function () {
+        $("#question").keyup(function () {
+            length = $(this).val().length;
+            console.log(length)
+            if (length > 33) {
+                $(this).css("font-size", "0.9em");
+            } else if (length > 25) {
+                $(this).css("font-size", "1.3em");
+            } else if (length > 18) {
+                $(this).css("font-size", "1.5em");
+            } else {
+                $(this).css("font-size", "2em");
+            }
+        });
+    });
 
     return {
         callTheHotline: callTheHotline
