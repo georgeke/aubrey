697ccfe644a75dc5151fc99116fe7e92bb9a39ab
George Ke <george-ke@hotmail.com>
2016-01-16 22:03:21 -0500

    ease-in-out blinking and better js code

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,12 +1,23 @@
-function callTheHotline() {
-    var question = $("#question").val();
+var app = (function ($) {
+    var DEFAULT_DESC = "Drake",
+        LOADING_DESC = "Loading...";
 
-    $.get("/hotline?question=" + question, function(data, status, response) {
-      if (response.readyState == 4 && response.status == 200) {
-        $("#bling").text(response.responseText);
-      }
-    });
+    function callTheHotline() {
+        event.preventDefault();
+        var question = $("#question").val();
 
-    event.preventDefault();
-    return false;
-}
+        $.get("/hotline?question=" + question, function(data, status, response) {
+            if (response.readyState == 4 && response.status == 200) {
+            $("#bling").text(response.responseText);
+            }
+        });
+
+        var description = $("#description")
+        description.text(LOADING_DESC);
+        description.addClass("flashing");
+    }
+
+    return {
+        callTheHotline: callTheHotline
+    }
+})(jQuery);
